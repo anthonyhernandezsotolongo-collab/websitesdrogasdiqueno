f46e0a4a3f9fdb82b04fbe6c9511627ce25cdbb7
escuela.noalasdrogas <anthonyhernandezsotolongo@gmail.com>
2026-06-08 23:53:03 -0400

    67

---
+++ html/javascript/drogas.js
@@ -0,0 +1,7 @@
+// Alternar menú hamburguesa
+const hamburger = document.getElementById("hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
